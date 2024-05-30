b1918f1410c333e72a48fafcd52b1aead3b81a03
YUSRIN20 <massyusrin123@gmail.com>
2024-05-31 00:19:00 +0530

    add Git repo button for portfolioSection

--- src/Components/portfolio/Portfolio.jsx
+++ src/Components/portfolio/Portfolio.jsx
@@ -8,22 +8,25 @@ const items = [
         id: 1,
         title: "Hotel Booking Web Application",
         img: "./HotelBooking.png",
-        demoLink:'https://hotel-booking-web.netlify.app/',
+        demoLink: 'https://hotel-booking-web.netlify.app/',
+        gitLink:"https://github.com/YUSRIN20/HotelBooking-Client-Side.git",
         desc: "Our hotel booking replica website is a comprehensive platform designed to simplify the process of finding and reserving accommodations. Users can effortlessly search for hotels based on location, date, and price range, view detailed information and photos of each property, and make secure bookings online. The website offers a user-friendly interface, real-time availability updates, and customizable search filters to ensure a seamless and personalized booking experience."
     },
     {
         id: 2,
         title: "Blog Web Application",
         img: "./blog.png",
-        demoLink:'https://simpleblog-website.netlify.app/',
+        demoLink: 'https://simpleblog-website.netlify.app/',
+        gitLink:"https://github.com/YUSRIN20/Blog-Client-Side.git",
         desc: "The blog application allows users to register, create, view, update, and delete blog posts. It features user authentication using JSON Web Tokens (JWT) for secure access. Users can categorize their posts and filter content based on categories. The application is built with Node.js, Express.js for the backend, MongoDB for database storage, and Mongoose for data modeling. It ensures data security with bcrypt-hashed passwords and includes error handling for input validation. Future enhancements could include user comments, likes, and improved search capabilities."
     },
-    
+
 
     {
         id: 3,
         title: "Bulk Email Tool",
-        demoLink:"https://bulk-email-tool-web.netlify.app/",
+        demoLink: "https://bulk-email-tool-web.netlify.app/",
+        gitLink:"https://github.com/YUSRIN20/Bulk-Email-Tool-Frontend.git",
         img: "BulkEmailTool.png",
         desc: "The Bulk Email Tool project comprises frontend and backend components for efficient email management. The frontend, built with React.js, handles user authentication, email composition, and analytics display. The backend, developed with Node.js, Express, and MongoDB, manages user registration, authentication, email sending, and analytics retrieval. With features like user-friendly interfaces, email composition, and analytics tracking, it provides a comprehensive solution for email management needs."
     },
@@ -37,8 +40,9 @@ const items = [
     {
         id: 4,
         title: "Login Management Web Application",
+        demoLink: 'https://password-reset-demo.netlify.app/',
+        gitLink:"https://github.com/YUSRIN20/Password-Reset-Task-FrontEnd.git",
         img: "./PasswordReset.png",
-        demoLink:'https://password-reset-demo.netlify.app/',
         desc: "This project offers a full password management solution with frontend and backend components. The frontend, built with React.js, handles user registration, login, and password reset. It integrates smoothly with the backend, developed in Node.js with Express.js and MongoDB for database management. The backend ensures secure authentication, password hashing, and email-based password reset functionality using Nodemailer. Demonstrating proficiency in full-stack development, it showcases seamless integration between frontend design and backend logic."
     },
     // {
@@ -65,14 +69,19 @@ const Single = ({ item }) => {
             <div className="container">
                 <div className="wrapper">
                     <div className="imageContainer" ref={ref}>
-                    <img src={item.img} alt="" />
+                        <img src={item.img} alt="" />
                     </div>
                     <motion.div className="textContainer" style={{ y }}>
                         <h2 >{item.title}</h2>
                         <p>{item.desc}</p>
-                        <button>
-                            <a href={item.demoLink} target='blank'>See Demo</a>
-                        </button>
+                        <div className='buttons'>
+                            <button>
+                                <a href={item.demoLink} target='blank'>See Demo <i class="fa-solid fa-eye"></i></a>
+                            </button>
+                            <button>
+                                <a href={item.gitLink} target='blank'>See Repo <i class="fa-brands fa-github"></i></a>
+                            </button>
+                        </div>
                     </motion.div>
                 </div>
             </div>
